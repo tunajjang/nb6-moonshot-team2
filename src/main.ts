a7e82fba462cc870fe3475e1ad4e61e3f059e7b2
Dongwon Jeong <tunajjang@gmail.com>
2025-12-22 13:07:28 +0900

    충돌 테스트 커밋

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 console.log('hello world');
 
 console.log('이것은 테스트용 코드입니다.');
+
+const mosebuger: number = 10000;
+const cola: number = 20000;
+
+const setmenu: number = mosebuger + cola;
+
+console.log(`세트메뉴는 ${setmenu}원 입니다`);
