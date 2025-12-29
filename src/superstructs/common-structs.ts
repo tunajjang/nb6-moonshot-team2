75273b4f0ecf075a2665c9ff2d6ae6ccfd4d22aa
Dongwon Jeong <tunajjang@gmail.com>
2025-12-29 18:00:19 +0900

    ✨ feat : Task API 구현 (일부 수정이 필요)

--- src/superstructs/common-structs.ts
+++ src/superstructs/common-structs.ts
@@ -3,5 +3,19 @@ import * as s from 'superstruct';
 const integerString = s.coerce(s.integer(), s.string(), (value) => parseInt(value));
 
 export const IdParamStruct = s.object({
-  id: integerString,
+  taskId: integerString,
+});
+
+export const ProjectIdParamStruct = s.object({
+  projectId: integerString,
+});
+
+export const PageParamsStrict = s.object({
+  page: s.defaulted(integerString, 1),
+  limit: s.defaulted(integerString, 10),
+  orderBy: s.defaulted(s.enums(['created_at', 'name', 'end_date']), 'created_at'),
+  order: s.defaulted(s.enums(['asc', 'desc']), 'desc'),
+  keyword: s.optional(s.nonempty(s.string())),
+  status: s.optional(s.enums(['PENDING', 'IN_PROGRESS', 'DONE'])),
+  assigneeId: s.optional(integerString),
 });
--- src/superstructs/task-struct.ts
+++ src/superstructs/task-struct.ts
@@ -1,35 +1,25 @@
 import * as s from 'superstruct';
+import { PageParamsStrict } from './common-structs';
 
 const TaskStatusStruct = s.enums(['PENDING', 'IN_PROGRESS', 'DONE']);
-const ParamOrderStruct = s.enums(['asc', 'desc']);
-const ParamOrderByStruct = s.enums([`created_at`, `name`, `end_date`]);
+
+export const GetTaskListParamsStruct = PageParamsStrict;
 
 export const CreateTaskBodyStruct = s.object({
   projectId: s.number(),
   title: s.nonempty(s.string()),
-  startAt: s.date(),
-  endAt: s.date(),
+  startAt: s.string(),
+  endAt: s.string(),
   status: TaskStatusStruct,
   assigneeId: s.number(),
   tagId: s.optional(s.array(s.number())),
   attachmentId: s.optional(s.array(s.number())),
 });
 
-export const TaskParamsStruct = s.object({
-  page: s.defaulted(s.number(), 1),
-  limit: s.defaulted(s.number(), 10),
-  status: s.optional(TaskStatusStruct),
-  order: s.optional(ParamOrderStruct),
-  orderBy: s.optional(ParamOrderByStruct),
-  assigneeId: s.optional(s.number()),
-  keyword: s.optional(s.nonempty(s.string())),
-});
-
 export const UpdateTaskBodyStruct = s.partial(CreateTaskBodyStruct);
 
 export type CreateTaskInput = s.Infer<typeof CreateTaskBodyStruct>;
 export type UpdateTaskInput = s.Infer<typeof UpdateTaskBodyStruct>;
-export type TaskParams = s.Infer<typeof TaskParamsStruct>;
 
 /*
 title: string;
@@ -42,15 +32,4 @@ title: string;
     id: number;
     projectId: number;
     assigneeId: number;
-*/
-
-/*
-Request params
-- page: `number`
-- limit: `number`
-- status: `todo` | `in_progress` | `done` (상태 필터)
-- assignee: `number` (담당자 필터)
-- keyword: `string` (검색어)
-- order:  `asc` | `desc`
-- order_by: `created_at` , `name` , `end_date`
 */
