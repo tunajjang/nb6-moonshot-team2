f07dd0960a84ec999ee3ce2cc8cae1618f7a58e8
Jueun Lee <wndms328@naver.com>
2026-01-05 11:21:09 +0900

    feat: 프로젝트 초대 시 이메일 발송 기능 추가
    - nodemailer 패키지 추가
    - EmailService 구현 (초대 이메일 발송)
    - MemberService에 초대 생성 시 이메일 발송 로직 통합
    - 초대 링크 URL 자동 생성
    - README에 이메일 설정 가이드 추가

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 export * from './auth.service';
 export * from './comment.service';
+export * from './email.service';
 export * from './member.service';
 export * from './project.service';
 export * from './user.service';
