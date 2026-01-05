abd31dd3a5a0a0149b7b065a27e7e9b8a4b8db61
Jueun Lee <wndms328@naver.com>
2026-01-05 14:38:04 +0900

    feat: 기본 에러 클래스 메시지 한국어로 변경
    - NotFoundError: 리소스를 찾을 수 없습니다
    - UnauthorizedError: 인증이 필요합니다
    - ForbiddenError: 접근이 거부되었습니다
    - BadRequestError: 잘못된 요청입니다
    - ConflictError: 충돌이 발생했습니다

--- dist/lib/errors/app.error.js
+++ dist/lib/errors/app.error.js
@@ -13,7 +13,7 @@ class AppError extends Error {
 exports.AppError = AppError;
 // 404 Not Found 에러
 class NotFoundError extends AppError {
-    constructor(message = 'Resource not found') {
+    constructor(message = '리소스를 찾을 수 없습니다') {
         super(message, 404);
         this.name = 'NotFoundError';
     }
@@ -21,7 +21,7 @@ class NotFoundError extends AppError {
 exports.NotFoundError = NotFoundError;
 // 401 Unauthorized 에러
 class UnauthorizedError extends AppError {
-    constructor(message = 'Unauthorized') {
+    constructor(message = '인증이 필요합니다') {
         super(message, 401);
         this.name = 'UnauthorizedError';
     }
@@ -29,7 +29,7 @@ class UnauthorizedError extends AppError {
 exports.UnauthorizedError = UnauthorizedError;
 // 403 Forbidden 에러
 class ForbiddenError extends AppError {
-    constructor(message = 'Forbidden') {
+    constructor(message = '접근이 거부되었습니다') {
         super(message, 403);
         this.name = 'ForbiddenError';
     }
@@ -37,7 +37,7 @@ class ForbiddenError extends AppError {
 exports.ForbiddenError = ForbiddenError;
 // 400 Bad Request 에러
 class BadRequestError extends AppError {
-    constructor(message = 'Bad request') {
+    constructor(message = '잘못된 요청입니다') {
         super(message, 400);
         this.name = 'BadRequestError';
     }
@@ -45,7 +45,7 @@ class BadRequestError extends AppError {
 exports.BadRequestError = BadRequestError;
 // 409 Conflict 에러
 class ConflictError extends AppError {
-    constructor(message = 'Conflict') {
+    constructor(message = '충돌이 발생했습니다') {
         super(message, 409);
         this.name = 'ConflictError';
     }
